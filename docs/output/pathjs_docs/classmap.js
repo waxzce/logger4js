e0b8ea3b488d8e27fc22ee5cf15299c4b1de5e61
Quentin ADAM <quentin.adam@waxzce.org>
2011-08-08 13:03:16 +0200

    docs + readme

---
+++ docs/output/pathjs_docs/classmap.js
@@ -0,0 +1,15 @@
+YAHOO.env.classMap = {"Logger": "logger4js", "Levels": "logger4js", "DefaultLoggerImpl": "logger4js"};
+
+YAHOO.env.resolveClass = function(className) {
+    var a=className.split('.'), ns=YAHOO.env.classMap;
+
+    for (var i=0; i<a.length; i=i+1) {
+        if (ns[a[i]]) {
+            ns = ns[a[i]];
+        } else {
+            return null;
+        }
+    }
+
+    return ns;
+};
